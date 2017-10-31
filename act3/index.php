3bcf59357efb7ee3275b4a68f3f24a981a654870
vluznuria <33235332+vluznuria@users.noreply.github.com>
2017-10-31 22:20:56 +0100

    Update index.php

--- act3/index.php
+++ act3/index.php
@@ -1,10 +1,12 @@
 <?php
   session_start();
-
+  
   if  (isset($_POST)&&
       !empty($_POST['numero'])){
           $numero=htmlspecialchars($_POST['numero']);
+          //aquí guardo en $numero el numero puesto en el formulario del html para guardarlo para trabajar mas adelante con el
           $_SESSION['numero']=$numero;
+          //en esta linea estoy vinculando este archivo con el index2.php porque tras pedir un numero saldrán en la otra pantalla
           header('Location:index2.php');
       }
 
@@ -20,7 +22,8 @@
    <body>
 
      <h1>Añade los numeros que quieras</h1>
-
+          <!--Este formulario me pide cuantas cajas para introducir numeros quiero para que en la pantalla siguiente me las imprima
+          en orden-->
      <form action="<?= $_SERVER['PHP_SELF'];?>" method="post">
        <p>Numero de cajas:<input type="number" name="numero"></p>
        <p><input type="submit" value="Cajas"></p>
